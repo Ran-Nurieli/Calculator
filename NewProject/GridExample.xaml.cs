aebae3bb0819b954e28fbc25573bc3271fbc54d6
User <User@LAB3-3>
2024-01-15 15:10:40 +0200

    aa

--- NewProject/GridExample.xaml.cs
+++ NewProject/GridExample.xaml.cs
@@ -6,4 +6,15 @@ public partial class GridExample : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Print(object sender, EventArgs e)
+    {
+        lbl_text.Text += sender.GetType();
+    }
+    private void NumPressed(object sender, EventArgs e)
+    {
+        Button btn = (Button)sender;
+        lbl_text.Text +=btn.Text;
+    }
+    
 }
